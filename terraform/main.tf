fe1fe9cd66b9323581c5d6b0d782c92e343d2099
NorD <nordazavr@gmail.com>
2022-02-20 20:13:11 +0300

    Before create instanses. All infra

---
+++ terraform/main.tf
@@ -0,0 +1,295 @@
+# AWS Task
+# 20220220 by NorD
+
+
+# Region settings
+provider "aws" { 
+  region = "eu-central-1"
+}
+
+# Get my IP
+module "myip" {
+  source  = "4ops/myip/http"
+  version = "1.0.0"
+} 
+
+# AZ
+data "aws_availability_zones" "available" { }
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+} 
+
+# Last ami
+data "aws_ami" "last_amazon"{
+  owners = ["137112412989"]
+  most_recent = true
+  filter{
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+# Create VPC
+resource "aws_vpc" "wp_vcp" {
+  cidr_block = "192.168.0.0/16"
+  enable_dns_hostnames = true 
+  tags = {
+    Name    = "WP_VPC"
+    Owner   = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+}
+
+# Create Subnets
+resource "aws_subnet" "subnet-1" {
+  vpc_id            = aws_vpc.wp_vcp.id
+  cidr_block        = "192.168.100.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name    = "Subnet in ${data.aws_availability_zones.available.names[0]}"
+    Owner   = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id            = aws_vpc.wp_vcp.id
+  cidr_block        = "192.168.200.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name    = "Subnet in ${data.aws_availability_zones.available.names[1]}"
+    Owner   = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+}
+
+# Create Internet Gateway for WP_VPC
+resource "aws_internet_gateway" "wp_gateway" {
+  vpc_id = aws_vpc.wp_vcp.id
+  tags = {
+    Name    = "WP_IG"
+    Owner   = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+  depends_on = [aws_vpc.wp_vcp]
+} 
+
+# Route to Internet Gateway
+resource "aws_route" "route_to_wp_gateway" {
+  route_table_id         = aws_vpc.wp_vcp.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.wp_gateway.id
+  depends_on             = [aws_internet_gateway.wp_gateway, aws_vpc.wp_vcp]
+} 
+
+# Create route association
+resource "aws_route_table_association" "subnet-1" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_vpc.wp_vcp.main_route_table_id
+}
+
+resource "aws_route_table_association" "subnet-2" {
+  subnet_id      = aws_subnet.subnet-2.id
+  route_table_id = aws_vpc.wp_vcp.main_route_table_id
+}
+
+# Create EFS
+resource "aws_efs_file_system" "wp_efs" {
+  encrypted = true
+  tags = {
+    Name = "WP_EFS"
+    Owner   = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+} 
+
+# Targets for EFS
+resource "aws_efs_mount_target" "subnet-1" {
+  file_system_id  = aws_efs_file_system.wp_efs.id
+  subnet_id       = aws_subnet.subnet-1.id
+  security_groups = [aws_security_group.sg_efs.id]
+  depends_on      = [aws_efs_file_system.wp_efs, aws_security_group.sg_efs]
+}
+
+resource "aws_efs_mount_target" "subnet-2" {
+  file_system_id  = aws_efs_file_system.wp_efs.id
+  subnet_id       = aws_subnet.subnet-2.id
+  security_groups = [aws_security_group.sg_efs.id]
+  depends_on      = [aws_efs_file_system.wp_efs, aws_security_group.sg_efs]
+}
+
+
+
+# Create Security Groups
+# For EC2 Instances 80, 443, 22
+resource "aws_security_group" "sg_ec2" {
+  name        = "SG_for_EC2"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.wp_vcp.id
+
+  dynamic ingress {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${module.myip.address}/32"] 
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG for EC2"
+    Owner = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+}
+
+# For EFS
+resource "aws_security_group" "sg_efs" {
+  name        = "SG_for_EFS"
+  description = "Allow NFS inbound traffic"
+  vpc_id      = aws_vpc.wp_vcp.id
+
+  ingress {
+    description     = "NFS from EC2"
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_ec2.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "SG for EFS"
+    Owner = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+
+  depends_on = [aws_security_group.sg_ec2]
+}
+
+# For RDS MySQL
+resource "aws_security_group" "sg_rds" {
+  name        = "SG_for_RDS"
+  description = "Allow MySQL inbound traffic"
+  vpc_id      = aws_vpc.wp_vcp.id
+
+  ingress {
+    description     = "RDS from EC2"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_ec2.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS from EC2"
+    Owner = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+
+  depends_on = [aws_security_group.sg_ec2]
+}
+
+# For ELB port 80
+resource "aws_security_group" "sg_elb" {
+  name        = "SG_for_ELB"
+  description = "Allow traffic for ELB"
+  vpc_id      = aws_vpc.wp_vcp.id
+
+  ingress {
+    description = "Allow all inbound traffic on the 80 port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_ec2.id]
+  }
+
+  tags = {
+    Name = "SG for ELB"
+    Owner = "Andrei Shcheglov"
+    Project = "AWS_Task"
+  }
+
+  depends_on = [aws_security_group.sg_ec2]
+} 
+
+
+# Crete subnet for RDS
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
+} 
+
+resource "aws_db_instance" "mysql" {
+  identifier                      = "mysql"
+  engine                          = "mysql"
+  engine_version                  = "5.7.33"
+  instance_class                  = "db.t2.micro"
+  db_subnet_group_name            = aws_db_subnet_group.default.name
+  enabled_cloudwatch_logs_exports = ["general", "error"]
+  name                            = var.rds_credentials.dbname
+  username                        = var.rds_credentials.username
+  password                        = var.rds_credentials.password
+  allocated_storage               = 20
+  max_allocated_storage           = 0
+  backup_retention_period         = 7
+  backup_window                   = "00:00-00:30"
+  maintenance_window              = "Sun:21:00-Sun:21:30"
+  storage_type                    = "gp2"
+  vpc_security_group_ids          = [aws_security_group.sg_rds.id]
+  skip_final_snapshot             = true
+  depends_on                      = [aws_security_group.sg_rds, aws_db_subnet_group.default]
+}
+ 
+
+
+
+
+# output "sg_ec2" {
+#   value = aws_security_group.sg_ec2.id
+# }
+
+
